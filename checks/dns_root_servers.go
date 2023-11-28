3d49dd6d42d7e905890de96d67aabb2304725d6e
Robert Kisteleki <robertk@ripe.net>
2023-11-28 14:04:20 +0100

    b-root DNS changed IPs

--- checks/dns_root_servers.go
+++ checks/dns_root_servers.go
@@ -21,19 +21,19 @@ type rootDNSServer struct {
 
 // these really don't change too often
 var rootDNSServers = []rootDNSServer{
-	{"A", serverAddress{"198.41.0.4", "2001:503:ba3e::2:30"}},      // Verisign, Inc.
-	{"B", serverAddress{"199.9.14.201", "2001:500:200::b"}},        // Information Sciences Institute (ISI)
-	{"C", serverAddress{"192.33.4.12", "2001:500:2::c"}},           // Cogent Communications
-	{"D", serverAddress{"199.7.91.13", "2001:500:2d::d"}},          // University of Maryland
-	{"E", serverAddress{"192.203.230.10", "2001:500:a8::e"}},       // NASA Ames Research Center
-	{"F", serverAddress{"192.5.5.241", "2001:500:2f::f"}},          // Internet Systems Consortium, Inc. (ISC)
-	{"G", serverAddress{"192.112.36.4", "2001:500:12::d0d"}},       // Defense Information Systems Agency
-	{"H", serverAddress{"198.97.190.53", "2001:500:1:0:0:0:0:53"}}, // U.S. Army Research Lab
-	{"I", serverAddress{"192.36.148.17", "2001:7fe::53"}},          // Netnod
-	{"J", serverAddress{"192.58.128.30", "2001:503:c27::2:30"}},    // Verisign, Inc.
-	{"K", serverAddress{"193.0.14.129", "2001:7fd::1"}},            // RIPE NCC
-	{"L", serverAddress{"199.7.83.42", "2001:500:9f::42"}},         // ICANN
-	{"M", serverAddress{"202.12.27.33", "2001:dc3::35"}},           // WIDE Project
+	{"A", serverAddress{"198.41.0.4", "2001:503:ba3e::2:30"}},   // Verisign, Inc.
+	{"B", serverAddress{"170.247.170.2", "2801:1b8:10::b"}},     // Information Sciences Institute (ISI)
+	{"C", serverAddress{"192.33.4.12", "2001:500:2::c"}},        // Cogent Communications
+	{"D", serverAddress{"199.7.91.13", "2001:500:2d::d"}},       // University of Maryland
+	{"E", serverAddress{"192.203.230.10", "2001:500:a8::e"}},    // NASA Ames Research Center
+	{"F", serverAddress{"192.5.5.241", "2001:500:2f::f"}},       // Internet Systems Consortium, Inc. (ISC)
+	{"G", serverAddress{"192.112.36.4", "2001:500:12::d0d"}},    // Defense Information Systems Agency
+	{"H", serverAddress{"198.97.190.53", "2001:500:1::53"}},     // U.S. Army Research Lab
+	{"I", serverAddress{"192.36.148.17", "2001:7fe::53"}},       // Netnod
+	{"J", serverAddress{"192.58.128.30", "2001:503:c27::2:30"}}, // Verisign, Inc.
+	{"K", serverAddress{"193.0.14.129", "2001:7fd::1"}},         // RIPE NCC
+	{"L", serverAddress{"199.7.83.42", "2001:500:9f::42"}},      // ICANN
+	{"M", serverAddress{"202.12.27.33", "2001:dc3::35"}},        // WIDE Project
 }
 
 // CheckDNSRootServers checks all DNS root servers
